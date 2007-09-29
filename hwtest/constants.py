2836313cc3d02ed6e490dd17258d5c91d9d9c495
Marc Tardif <marc.tardif@canonical.com>
2007-09-29 16:46:56 -0400

    Fixed path to SHARE_DIR.

--- hwtest/constants.py
+++ hwtest/constants.py
@@ -7,4 +7,4 @@ MACHINE_ID_HEADER = "X-Machine-ID"
 MESSAGE_API_HEADER = "X-Message-API"
 
 # Directory containing shared files.
-SHARE_DIR = os.environ.get("SHARE_DIR")
+SHARE_DIR = os.environ.get("SHARE_DIR") or '/usr/share/hwtest'
