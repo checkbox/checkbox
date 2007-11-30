99de7c9369350665e7ceb49da29a013d20e21047
Marc Tardif <marc.tardif@canonical.com>
2007-11-30 14:56:53 -0500

    Fixed irecursive_update to work on python 2.4 and 2.5. The problem is that the behavior of an iterator in a boolean context has changed.

--- hwtest/lib/update.py
+++ hwtest/lib/update.py
@@ -1,23 +1,24 @@
 def recursive_update(dst, src):
-    irecursive_update(dst, src.iteritems())
+    irecursive_update(dst, list(src.iteritems()))
     return dst
 
-def irecursive_update(a, biter):
+def irecursive_update(a, blist):
     try:
         stack = []
-        while biter:
-            for (bk,bv) in biter:
+        while blist:
+            while blist:
+                (bk, bv) = blist.pop(0)
                 if (bk in a 
-                    and isinstance(bv, dict)
-                    and isinstance(a[bk], dict)):
-                    stack.append((biter, a)) # current -> parent
+                     and isinstance(bv, dict)
+                     and isinstance(a[bk], dict)):
+                    stack.append((blist, a)) # current -> parent
                     break
                 else:
                     a[bk] = bv
             else:
-                while not biter:
-                    biter, a = stack.pop() # current <- parent 
+                while not blist:
+                    blist, a = stack.pop() # current <- parent 
                 continue
-            biter, a = bv.iteritems(), a[bk]
+            blist, a = list(bv.iteritems()), a[bk]
     except IndexError:
         pass
