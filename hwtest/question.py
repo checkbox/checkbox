a845e67b119a2542287232a32dd63ec2358822cd
Marc Tardif <marc.tardif@canonical.com>
2007-12-07 12:31:39 -0500

    Fixed minor problems reported by pyflakes and pylint.

--- hwtest/question.py
+++ hwtest/question.py
@@ -1,6 +1,13 @@
+"""
+The purpose of a question is to encapsulate the concept of a test
+which might be presented in several ways. For example, a question might
+require manual intervention whereas another question might be completely
+automatic. Either way, this module provides the base common to each type
+of question.
+"""
+
 import os
 import re
-import types
 import logging
 
 from StringIO import StringIO
@@ -42,15 +49,15 @@ class QuestionParser(object):
 
         logging.info("Loading question properties for: %s", properties["name"])
 
-        if filter(lambda q: q["name"] == properties["name"], self.questions):
+        if [q for q in self.questions if q["name"] == properties["name"]]:
             raise Exception, \
                 "Question %s already has a question of the same name." \
                 % properties["name"]
 
         self.questions.append(properties)
 
-    def _load_descriptor(self, fd, name):
-        for string in reader(fd):
+    def _load_descriptor(self, descriptor, name):
+        for string in reader(descriptor):
             if not string:
                 break
 
@@ -65,7 +72,8 @@ class QuestionParser(object):
                 if field:
                     if properties.has_key(field):
                         raise Exception, \
-                            "Path %s has a duplicate field '%s' with a new value '%s'." \
+                            "Path %s has a duplicate field '%s'" \
+                            " with a new value '%s'." \
                             % (name, field, value)
                     properties[field] = value or extended
 
@@ -79,7 +87,6 @@ class QuestionParser(object):
                     field = match.groups()[0].lower()
                     value = match.groups()[1].rstrip()
                     extended = ''
-                    basefield = re.sub(r"-.+$", "", field)
                     continue
 
                 if re.search(r"^\s\.$", line):
@@ -110,18 +117,27 @@ class QuestionParser(object):
             _save(field, value, extended, name)
             self._load_properties(**properties)
 
-    def load_string(self, str):
+    def load_string(self, string):
+        """
+        Parse questions from given string.
+        """
         logging.info("Loading question from string")
-        fd = StringIO(str)
-        self._load_descriptor(fd, "string")
+        descriptor = StringIO(string)
+        self._load_descriptor(descriptor, "string")
 
     def load_path(self, path):
+        """
+        Parse questions from filename in given path.
+        """
         logging.info("Loading question from path: %s", path)
 
-        fd = file(path, "r")
-        self._load_descriptor(fd, path)
+        descriptor = file(path, "r")
+        self._load_descriptor(descriptor, path)
 
     def load_directory(self, directory):
+        """
+        Parse questions from each filename in the given directory.
+        """
         logging.info("Loading questions from directory: %s", directory)
         for name in [name for name in os.listdir(directory)
                      if name.endswith(".txt")]:
@@ -151,20 +167,29 @@ class QuestionManager(object):
         example, the requires field will be evaluated and the question
         will be skipped if this fails.
         """
-        def dependent_repeat_func(question, resolver):
+        def dependent_prerepeat_func(question, resolver):
+            """Pre repeater function which assigns the SKIP status to
+               dependents when a question has a status of NO or SKIP."""
             answer = question.answer
             if answer and (answer.status == NO or answer.status == SKIP):
                 for dependent in resolver.get_dependents(question):
                     dependent.set_answer(SKIP, auto=True)
 
         def requires_exclude_func(question):
+            """Excluder function which removes question when the requires
+               field exists and doesn't meet the given requirements."""
             return isinstance(question.requires, list) \
                    and len(question.requires) == 0
 
         def answered_exclude_next_func(question):
+            """Excluder function which is called when clicking on the next
+               button to skip questions which have been answered already."""
             return question.answer != None
 
         def answered_exclude_prev_func(question):
+            """Excluder function which is called when clicking on the
+               previous button when the question has been automatically
+               answered."""
             if question.answer and question.answer.auto == True:
                 question.answer = None
                 return True
@@ -172,7 +197,7 @@ class QuestionManager(object):
                 return False
 
         resolver = Resolver()
-        question_dict = dict((question.name, question) for question in self._questions)
+        question_dict = dict((q.name, q) for q in self._questions)
         for question in self._questions:
             question_depends = [question_dict[d] for d in question.depends]
             resolver.add(question, *question_depends)
@@ -181,7 +206,7 @@ class QuestionManager(object):
         questions_iter = Iterator(questions)
         questions_iter = PreRepeater(questions_iter,
             lambda question, resolver=resolver: \
-                   dependent_repeat_func(question, resolver))
+                   dependent_prerepeat_func(question, resolver))
         questions_iter = Excluder(questions_iter,
             requires_exclude_func, requires_exclude_func)
         questions_iter = Excluder(questions_iter,
@@ -235,8 +260,8 @@ class Question(object):
 
     def get_properties(self):
         properties = {}
-        for f in Question.required_fields + Question.optional_fields.keys():
-            properties[f] = self.properties[f]
+        for field in Question.required_fields + Question.optional_fields.keys():
+            properties[field] = self.properties[field]
         if self.answer:
             properties['answer'] = self.answer.get_properties()
         return properties
@@ -254,7 +279,7 @@ class Question(object):
             if not self.properties.has_key(field):
                 raise Exception, \
                     "Question properties does not contain a '%s': %s" \
-                    % (field, properties)
+                    % (field, self.properties)
 
         # Typed fields
         for field in ["architectures", "categories", "depends"]:
