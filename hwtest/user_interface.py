7ea579e8d9eed8d4ac5a3aec1ad4461856f432c4
Marc Tardif <marc.tardif@canonical.com>
2007-12-10 22:16:26 -0500

    Moved user interface plugin base class to an actual plugin.

--- hwtest/user_interface.py
+++ hwtest/user_interface.py
@@ -1,8 +1,4 @@
-import sys
 import gettext
-
-from hwtest.plugin import Plugin
-from hwtest.contrib.REThread import REThread
 
 
 class UserInterface(object):
@@ -31,59 +27,3 @@ class UserInterface(object):
 
     def show_error_message(self, error):
         raise NotImplementedError, "this function must be overridden by subclasses"
-
-
-class UserInterfacePlugin(Plugin):
-
-    def __init__(self, config, user_interface):
-        super(UserInterfacePlugin, self).__init__(config)
-        self._user_interface = user_interface
-
-    def register(self, manager):
-        super(UserInterfacePlugin, self).register(manager)
-        self._manager.reactor.call_on(("interface", "show-category"), self.show_category)
-        self._manager.reactor.call_on(("interface", "show-question"), self.show_question)
-        self._manager.reactor.call_on(("interface", "show-gather"), self.show_gather)
-        self._manager.reactor.call_on(("interface", "show-exchange"), self.show_exchange)
-        self._manager.reactor.call_on(("interface", "show-error-message"), self.show_error_message)
-
-    def show_category(self, category=None):
-        if not category:
-            category = self._user_interface.show_category()
-        self._manager.reactor.fire(("prompt", "set-category"), category)
-
-    def show_question(self, question, has_prev, has_next):
-        direction = self._user_interface.show_question(question, has_prev,
-                                                       has_next)
-        self._manager.reactor.fire(("prompt", "set-direction"), direction)
-
-    def do_gather(self):
-        self._manager.reactor.fire("gather")
-
-    def show_gather(self):
-        self._user_interface.show_gather()
-        thread = REThread(target=self.do_gather, name="do_gather")
-        thread.start()
-        while thread.isAlive():
-            self._user_interface.pulse_gather()
-            try:
-                thread.join(0.1)
-            except KeyboardInterrupt:
-                sys.exit(1)
-        thread.exc_raise()
-
-    def show_exchange(self, email=None):
-        # Prompt for email the first time unless it is provided.
-        if not email:
-            email = self._user_interface.show_exchange()
-        while True:
-            self._manager.reactor.fire(("report", "email"), email)
-            self._manager.reactor.fire("exchange")
-            error = self._manager.get_error()
-            if not error:
-                break
-            # Always prompt for the email subsequent times.
-            email = self._user_interface.show_exchange(error)
-
-    def show_error_message(self, title, text):
-        self._user_interface.show_error_message(title, text)
