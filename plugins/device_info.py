e3ad7d6da763d86c2cd1ccc571709aa7405a4889
Marc Tardif <marc.tardif@canonical.com>
2007-12-10 22:19:32 -0500

    Inlined not so useful create_message method.

--- plugins/device_info.py
+++ plugins/device_info.py
@@ -8,14 +8,10 @@ class DeviceInfo(Plugin):
         self._manager.reactor.call_on("gather", self.gather)
 
     def gather(self):
-        message = self.create_message()
-        self._manager.reactor.fire(("report", "device"), message)
-
-    def create_message(self):
         message = {}
         message["version"] = self._manager.registry.hald.version
         message["devices"] = self._manager.registry.hal
+        self._manager.reactor.fire(("report", "device"), message)
 
-        return message
 
 factory = DeviceInfo
