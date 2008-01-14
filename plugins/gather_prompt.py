a26e3031b26aaf7dedcaed01a44e39a256d40ab2
Marc Tardif <marc.tardif@canonical.com>
2008-01-14 11:34:41 -0500

    Added state for new concept of direction.

--- plugins/gather_prompt.py
+++ plugins/gather_prompt.py
@@ -5,11 +5,15 @@ class GatherPrompt(Plugin):
 
     def register(self, manager):
         super(GatherPrompt, self).register(manager)
+        self._done = False
+
         self._manager.reactor.call_on(("prompt", "gather"), self.prompt_gather)
  
     def prompt_gather(self, interface):
-        interface.do_wait(lambda: self._manager.reactor.fire("gather"),
-            "Gathering information from your system...")
+        if not self._done:
+            interface.do_wait(lambda: self._manager.reactor.fire("gather"),
+                "Gathering information from your system...")
+            self._done = True
 
 
 factory = GatherPrompt
--- plugins/report_prompt.py
+++ plugins/report_prompt.py
@@ -1,4 +1,5 @@
 from hwtest.plugin import Plugin
+from hwtest.iterator import PREV
 
 
 class ReportPrompt(Plugin):
@@ -8,6 +9,9 @@ class ReportPrompt(Plugin):
         self._manager.reactor.call_on(("prompt", "report"), self.prompt_report)
  
     def prompt_report(self, interface):
+        if interface.direction == PREV:
+            return
+
         # This could show a progress bar but it's very fast since most
         # information is available from the gather event type.
         self._manager.reactor.fire("report")
