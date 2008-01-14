787eaed1d544e50b4570448fa02a40f1a4131c27
Marc Tardif <marc.tardif@canonical.com>
2008-01-13 23:25:18 -0500

    Renamed ("interface", "snow-*") to ("prompt", "*") to match plugin names.

--- plugins/intro_prompt.py
+++ plugins/intro_prompt.py
@@ -1,14 +1,16 @@
 from hwtest.plugin import Plugin
+from hwtest.iterator import NEXT
 
 
 class IntroPrompt(Plugin):
 
     def register(self, manager):
         super(IntroPrompt, self).register(manager)
-        self._manager.reactor.call_on(("interface", "show-intro"), self.show_intro)
+        self._manager.reactor.call_on(("prompt", "intro"), self.prompt_intro)
 
-    def show_intro(self, interface):
-        intro = interface.show_intro()
+    def prompt_intro(self, interface):
+        interface.show_intro()
+        self._manager.reactor.fire(("interface", "direction"), NEXT)
 
 
 factory = IntroPrompt
