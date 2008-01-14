687f9ec6c85aec05e71ba6265a2f5c12a7dcded3
Marc Tardif <marc.tardif@canonical.com>
2008-01-14 11:27:18 -0500

    Added concept of direction to user interface.

--- hwtest/user_interface.py
+++ hwtest/user_interface.py
@@ -2,6 +2,7 @@ import sys
 import gettext
 
 from hwtest.contrib.REThread import REThread
+from hwtest.iterator import NEXT
 
 
 class UserInterface(object):
@@ -15,6 +16,7 @@ class UserInterface(object):
 
         self.application = None
         self.questions = None
+        self.direction = NEXT
 
         self.gettext_domain = "hwtest"
         gettext.textdomain(self.gettext_domain)
@@ -30,8 +32,12 @@ class UserInterface(object):
             except KeyboardInterrupt:
                 sys.exit(1)
         thread.exc_raise()
+        self.show_wait_end()
 
     def show_wait(self, message):
+        raise NotImplementedError, "this function must be overridden by subclasses"
+
+    def show_wait_end(self):
         raise NotImplementedError, "this function must be overridden by subclasses"
 
     def show_pulse(self):
@@ -40,13 +46,13 @@ class UserInterface(object):
     def show_intro(self):
         raise NotImplementedError, "this function must be overridden by subclasses"
 
-    def show_category(self):
+    def show_category(self, category):
         raise NotImplementedError, "this function must be overridden by subclasses"
 
     def show_question(self, question, has_prev, has_next):
         raise NotImplementedError, "this function must be overridden by subclasses"
 
-    def show_exchange(self, message, error):
+    def show_exchange(self, authentication, message, error):
         raise NotImplementedError, "this function must be overridden by subclasses"
 
     def show_final(self, message):
