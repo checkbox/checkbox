c659e01d97eba267b4a5a1d29b911c7b92ce5e1f
Marc Tardif <marc.tardif@canonical.com>
2008-01-14 11:36:33 -0500

    Added new concept of direction.

--- plugins/user_interface.py
+++ plugins/user_interface.py
@@ -1,28 +1,34 @@
 from hwtest.plugin import Plugin
+from hwtest.iterator import Iterator
 
 
 class UserInterface(Plugin):
 
     attributes = ["interface_module", "interface_class"]
 
-    def __init__(self, *args, **kwargs):
-        super(UserInterface, self).__init__(*args, **kwargs)
+    def run(self):
         interface_module = __import__(self.config.interface_module,
             None, None, [''])
         interface_class = getattr(interface_module, self.config.interface_class)
-        self.interface = interface_class(self.config)
+        interface = interface_class(self.config)
 
-    def run(self):
-        for event_type in [
-             ("interface", "show-permission"),
-             ("interface", "show-intro"),
-             ("interface", "show-gather"),
-             ("interface", "show-category"),
-             ("interface", "show-question"),
-             ("interface", "show-report"),
-             ("interface", "show-exchange"),
-             ("interface", "show-final")]:
-            self._manager.reactor.fire(event_type, self.interface)
+        iterator = Iterator([
+             ("prompt", "permission"),
+             ("prompt", "intro"),
+             ("prompt", "gather"),
+             ("prompt", "category"),
+             ("prompt", "questions"),
+             ("prompt", "report"),
+             ("prompt", "exchange"),
+             ("prompt", "final")])
+
+        while True:
+            try:
+                event_type = iterator.go(interface.direction)
+            except StopIteration:
+                break
+
+            self._manager.reactor.fire(event_type, interface)
 
 
 factory = UserInterface
