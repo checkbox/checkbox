be15201b1a954ab04fbed51e0cec40111fda9c07
Marc Tardif <marc.tardif@canonical.com>
2008-01-14 11:32:46 -0500

    Persisting category for new concept of direction.

--- plugins/category_prompt.py
+++ plugins/category_prompt.py
@@ -8,35 +8,36 @@ class CategoryPrompt(Plugin):
 
     def register(self, manager):
         super(CategoryPrompt, self).register(manager)
-        self._manager.reactor.call_on(("prompt", "category"), self.prompt_category)
+        self._category = self.config.category
+
+        for (rt, rh) in [
+             (("prompt", "category"), self.prompt_category)]:
+            self._manager.reactor.call_on(rt, rh)
 
     def prompt_category(self, interface):
-        category = self.config.category
         registry = self._manager.registry
 
         # Try to determine category from HAL formfactor
-        if not category:
+        if not self._category:
             formfactor = registry.hal.computer.system.formfactor
             if formfactor != "unknown":
-                category = formfactor
+                self._category = formfactor
 
         # Try to determine category from dpkg architecture
-        if not category:
+        if not self._category:
             architecture = registry.dpkg.architecture
             if architecture == "sparc":
-                category = "server"
+                self._category = "server"
 
         # Try to determine category from kernel version
-        if not category:
+        if not self._category:
             version = registry.hal.computer.system.kernel.version
             if version.endswith("-server"):
-                category = "server"
+                self._category = "server"
 
-        if not category:
-            category = interface.show_category()
+        self._category = interface.show_category(self._category)
 
-        self._manager.reactor.fire(("interface", "category"), category)
-        self._manager.reactor.fire(("interface", "direction"), NEXT)
+        self._manager.reactor.fire(("interface", "category"), self._category)
 
 
 factory = CategoryPrompt
