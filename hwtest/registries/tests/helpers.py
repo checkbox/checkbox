ce252b9ef7caebb4f40264cd56767875437a1a4e
Marc Tardif <marc.tardif@canonical.com>
2008-01-19 16:36:58 -0500

    Changed helpers so that it can be reused by other packages.

--- hwtest/registries/tests/helpers.py
+++ hwtest/registries/tests/helpers.py
@@ -5,10 +5,14 @@ from hwtest.config import Config
 
 class TestHelper(unittest.TestCase):
 
+    config_path = "./examples/hwtest.conf"
+    config_section = "hwtest/registries"
+
     def setUp(self):
         section_test_class_name = self.__class__.__name__
         section_class_name = section_test_class_name.replace("Test", "")
         section_name = section_class_name.replace("Registry", "").lower()
 
-        config = Config("./examples/hwtest.conf")
-        self.config = config.get_section("hwtest/registries/%s" % section_name)
+        config = Config(self.config_path)
+        self.config = config.get_section("%s/%s" \
+            % (self.config_section, section_name))
