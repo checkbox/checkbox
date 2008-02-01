17be3be22c56edc86b257ff16ec7b05c768b70f7
Marc Tardif <marc.tardif@canonical.com>
2008-02-01 12:24:19 -0500

    Added conversion for expressing numbers in human readable format.

--- hwtest/lib/conversion.py
+++ hwtest/lib/conversion.py
@@ -3,21 +3,39 @@ import re
 
 def string_to_type(string):
     conversion_table = {
-        '(yes|true)': lambda v: True,
-        '(no|false)': lambda v: False,
-        '\d+.\d+': lambda v: float(v.group(0)),
-        '(\d+)( b)?': lambda v: int(v.group(1)),
-        '(\d+) kb': lambda v: int(v.group(1)) * 1024,
-        '(\d+) mb': lambda v: int(v.group(1)) * 1024 * 1024,
-        '(\d+) gb': lambda v: int(v.group(1)) * 1024 * 1024 * 1024,
-        '(\d+)( hz)?': lambda v: int(v.group(1)),
-        '(\d+) khz': lambda v: int(v.group(1)) * 1024,
-        '(\d+) mhz': lambda v: int(v.group(1)) * 1024 * 1024,
-        '(\d+) ghz': lambda v: int(v.group(1)) * 1024 * 1024 * 1024}
+        "(yes|true)": lambda v: True,
+        "(no|false)": lambda v: False,
+        "\d+.\d+": lambda v: float(v.group(0)),
+        "(\d+)( b)?": lambda v: int(v.group(1)),
+        "(\d+) kb": lambda v: int(v.group(1)) * 1024,
+        "(\d+) mb": lambda v: int(v.group(1)) * 1024 * 1024,
+        "(\d+) gb": lambda v: int(v.group(1)) * 1024 * 1024 * 1024,
+        "(\d+)( hz)?": lambda v: int(v.group(1)),
+        "(\d+) khz": lambda v: int(v.group(1)) * 1024,
+        "(\d+) mhz": lambda v: int(v.group(1)) * 1024 * 1024,
+        "(\d+) ghz": lambda v: int(v.group(1)) * 1024 * 1024 * 1024}
 
     for regex, func in conversion_table.items():
-        match = re.match('^%s$' % regex, string, re.IGNORECASE)
+        match = re.match("^%s$" % regex, string, re.IGNORECASE)
         if match:
             return func(match)
+
+    return string
+
+def sizeof_bytes(bytes):
+    for x in ["bytes", "KB", "MB", "GB", "TB"]:
+        string = "%3.1f%s" % (bytes, x)
+        if bytes < 1024.0:
+            break
+        bytes /= 1024.0
+
+    return string
+
+def sizeof_hertz(hertz):
+    for x in ["Hz", "KHz", "MHz", "GHz"]:
+        string = "%3.1f%s" % (hertz, x)
+        if hertz < 1000.0:
+            break
+        hertz /= 1000.0
 
     return string
