5ea698fb584fa1f768cf2d50bb2204af899b3f7f
Marc Tardif <marc.tardif@canonical.com>
2008-02-27 23:09:23 -0500

    Added progress bar for building the report.

--- plugins/report_prompt.py
+++ plugins/report_prompt.py
@@ -1,3 +1,5 @@
+from gettext import gettext as _
+
 from hwtest.plugin import Plugin
 from hwtest.iterator import PREV
 
@@ -9,12 +11,9 @@ class ReportPrompt(Plugin):
         self._manager.reactor.call_on(("prompt", "report"), self.prompt_report)
  
     def prompt_report(self, interface):
-        if interface.direction == PREV:
-            return
-
-        # This could show a progress bar but it's very fast since most
-        # information is available from the gather event type.
-        self._manager.reactor.fire("report")
+        if interface.direction != PREV:
+            interface.show_wait(_("Building report..."),
+                lambda: self._manager.reactor.fire("report"))
 
 
 factory = ReportPrompt
