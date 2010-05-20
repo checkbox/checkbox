5dd1c1247324ebbb1501526d4fb7c402e0ab53c1
Javier Collado <javier.collado@canonical.com>
2010-05-20 11:00:22 +0200

    urwid interface skeleton files added

---
+++ checkbox_urwid/urwid_interface.py
@@ -0,0 +1,24 @@
+#
+# This file is part of Checkbox.
+#
+# Copyright 2008 Canonical Ltd.
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+#
+from checkbox.user_interface import (UserInterface, ANSWER_TO_STATUS,
+    ALL_ANSWERS, YES_ANSWER, NO_ANSWER, SKIP_ANSWER)
+
+
+class UrwidInterface(UserInterface):
+    pass
