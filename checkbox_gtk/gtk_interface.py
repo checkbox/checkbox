274f345ae5124fedb034037fee1d428ee67c29f1
Daniel Manrique <daniel.manrique@canonical.com>
2011-06-15 17:42:39 -0400

    Put test name as part of the window title, as an aid to reporting/debugging (LP: #744190)

--- checkbox_gtk/gtk_interface.py
+++ checkbox_gtk/gtk_interface.py
@@ -81,9 +81,10 @@ class GTKInterface(UserInterface):
         color = Gdk.color_parse("white")[1]
         eventbox_head.modify_bg(Gtk.StateType.NORMAL, color)
 
-        # Set dialog title
+        # Set and apply initial/default dialog title
+        self._app_title = title
         self._dialog = self._get_widget("dialog_main")
-        self._dialog.set_title(title)
+        self._dialog.set_title(self._app_title)
 
         #Setup and handler to render pixmap and translatable description text.
         self.IMAGE_HEAD_BACKGROUND = posixpath.join(data_path,
@@ -203,6 +204,12 @@ class GTKInterface(UserInterface):
 
         return state
 
+    def _set_main_title(self, test_name=""):
+        title = self._app_title
+        if test_name:
+            title+=" - %s" % (test_name) 
+        self._get_widget("dialog_main").set_title(title)
+
     def _run_dialog(self, dialog=None):
         def on_dialog_response(dialog, response, self):
             # Keep dialog alive when the button that has been clicked
@@ -240,6 +247,8 @@ class GTKInterface(UserInterface):
 
     @GTKHack
     def show_text(self, text, previous=None, next=None):
+        #Reset window title 
+        self._set_main_title()
         # Set buttons
         previous_state = self._set_button("button_previous", previous)
         next_state = self._set_button("button_next", next)
@@ -256,6 +265,8 @@ class GTKInterface(UserInterface):
 
     @GTKHack
     def show_entry(self, text, value, previous=None, next=None):
+        #Reset window title
+        self._set_main_title()
         # Set buttons
         previous_state = self._set_button("button_previous", previous)
         next_state = self._set_button("button_next", next)
@@ -358,6 +369,9 @@ class GTKInterface(UserInterface):
 
     @GTKHack
     def show_tree(self, text, options={}, default={}):
+        #Reset window title
+        self._set_main_title()
+
         (COLUMN_TEXT, COLUMN_ACTIVE) = range(2)
 
         # Set buttons
@@ -474,8 +488,12 @@ class GTKInterface(UserInterface):
 
     @GTKHack
     def show_test(self, test, runner):
+        #Set window title to reflect current test
+        self._set_main_title(test["name"])
+        
         self._set_sensitive("button_test", False)
         self._notebook.set_current_page(2)
+
 
         # Set test description
         if re.search(r"\$output", test["description"]):
