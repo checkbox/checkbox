93ecc0d3e57374e9688af4e3934365489c70a295
Brendan Donegan <brendan.donegan@canonical.com>
2012-02-28 11:09:34 +0000

    Added cciss as a valid bus ID for determining product in udevadm.py

--- checkbox/parsers/udevadm.py
+++ checkbox/parsers/udevadm.py
@@ -345,7 +345,7 @@ class UdevadmDevice:
                     return decode_id(device._environment["ID_MODEL_ENC"])
 
         if self._environment.get("DEVTYPE") == "disk" \
-           and self._environment.get("ID_BUS") in ("ata", "scsi"):
+           and self._environment.get("ID_BUS") in ("ata", "scsi","cciss"):
             return decode_id(self._environment["ID_MODEL_ENC"])
 
         # floppy
