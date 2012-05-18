bd3fb82be7e3bf37be5da609bbc54c5039553aab
Javier Collado <javier.collado@canonical.com>
2012-05-18 17:46:10 +0200

    Fixed make sure that value is a string in show_entry method

--- checkbox_qt/qt_interface.py
+++ checkbox_qt/qt_interface.py
@@ -128,6 +128,10 @@ class QTInterface(UserInterface):
                                 .format(m.group(1), m.group(2)),
                       text)
 
+        # Make sure that value is a string
+        if value is None:
+            value = ''
+
         self.qtiface.showEntry(text, value, label)
         self.wait_on_signals(submitTestsClicked=onSubmitTestsClicked)
         return self.qtiface.getSubmissionData()
