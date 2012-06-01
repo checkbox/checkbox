51c6eb8afac60847ff0e5bdba795bf8481347406
Marc Tardif <marc.tardif@canonical.com>
2012-06-01 17:46:18 -0400

    Migrated the xml_report module to support bytes and str instead of str and unicode respectively.

--- checkbox/reports/xml_report.py
+++ checkbox/reports/xml_report.py
@@ -36,10 +36,9 @@ class XmlReport(Report):
     def register_dumps(self):
         for (dt, dh) in [(bool, self.dumps_bool),
                          (int, self.dumps_int),
-                         (int, self.dumps_int),
                          (float, self.dumps_float),
+                         (bytes, self.dumps_bytes),
                          (str, self.dumps_str),
-                         (str, self.dumps_unicode),
                          (list, self.dumps_list),
                          (tuple, self.dumps_list),
                          (dict, self.dumps_dict),
@@ -52,8 +51,8 @@ class XmlReport(Report):
                          ("int", self.loads_int),
                          ("long", self.loads_int),
                          ("float", self.loads_float),
+                         ("bytes", self.loads_bytes),
                          ("str", self.loads_str),
-                         ("unicode", self.loads_str),
                          ("list", self.loads_list),
                          ("value", self.loads_value),
                          ("property", self.loads_property),
@@ -78,11 +77,11 @@ class XmlReport(Report):
     def dumps_float(self, obj, parent):
         self._dumps_text(str(obj), parent, "float")
 
+    def dumps_bytes(self, obj, parent):
+        self._dumps_text(obj, parent, "bytes")
+
     def dumps_str(self, obj, parent):
         self._dumps_text(obj, parent, "str")
-
-    def dumps_unicode(self, obj, parent):
-        self._dumps_text(obj, parent, "unicode")
 
     def dumps_list(self, obj, parent):
         parent.setAttribute("type", "list")
@@ -130,8 +129,11 @@ class XmlReport(Report):
     def loads_float(self, node):
         return float(node.data)
 
+    def loads_bytes(self, node):
+        return node.data.strip().encode("utf-8")
+
     def loads_str(self, node):
-        return str(node.data.strip())
+        return node.data.strip()
 
     def loads_list(self, node):
         nodes = []
