3f53f9ab7a3b888f1e2efe051c451dca9e85ca9c
Marc Tardif <marc.tardif@canonical.com>
2012-06-01 09:41:20 -0400

    Added the tz module instead of depending on the dateutil package.

--- checkbox/lib/conversion.py
+++ checkbox/lib/conversion.py
@@ -18,11 +18,12 @@
 #
 import re
 
-from dateutil import tz
 from datetime import (
     datetime,
     timedelta,
     )
+
+from checkbox.lib.tz import tzutc
 
 
 DATETIME_RE = re.compile(r"""
@@ -108,8 +109,7 @@ def string_to_datetime(string):
         milliseconds = 999999
 
     dt = datetime(
-        year, month, day, hour, minute, second, milliseconds,
-        tzinfo=tz.tzutc())
+        year, month, day, hour, minute, second, milliseconds, tzinfo=tzutc)
 
     tz_sign = time_parts['tz_sign']
     tz_hour = time_parts['tz_hour']
