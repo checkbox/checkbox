8ac0a9a235fbeb7897e32e4ae4d94885e9447f26
Marc Tardif <marc.tardif@canonical.com>
2012-06-01 09:30:27 -0400

    Migrated assignment to keywords which is no longer supported.

--- checkbox/contrib/glock.py
+++ checkbox/contrib/glock.py
@@ -70,11 +70,6 @@ class LockAlreadyAcquired(GlobalLockError):
     pass
 
 
-# Constants
-# ---------:
-if sys.version[:3] < '2.2':
-    True, False = 1, 0  # built-in in Python 2.2+
-
 #----------------------------------------------------------------------------
 class GlobalLock:
 #----------------------------------------------------------------------------
