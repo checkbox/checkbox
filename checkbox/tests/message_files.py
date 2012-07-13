5a7d18cf7ceafd4e667e1ffdc65b168f49eb2ceb
Daniel Manrique <daniel.manrique@canonical.com>
2012-07-12 22:46:03 -0400

    Changes suggested by Marc to test the already-merged message files on package building only.
    If the test detects it's being run at package build time, it will verify that all already-merged job files are valid at that time.
      
    Otherwise, it does a quick test on the pre-merged language files only.

--- checkbox/tests/message_files.py
+++ checkbox/tests/message_files.py
@@ -19,33 +19,25 @@
 import unittest
 from checkbox.lib.path import path_expand_recursive
 from checkbox.lib.template_i18n import TemplateI18n
-import tempfile
-import sys
 import os
+import shutil
 import subprocess
+import sys
+import tempfile
 
 
 class MessageFileFormatTest(unittest.TestCase):
 
     name = "message file format test"
 
-    def i18n_merge_jobs(self, dest_dir):
-        for filename in path_expand_recursive("./jobs"):
-            if not filename.endswith("~") :
-                subprocess.call(['intltool-merge',
-                                '-r',
-                                '-q',
-                                '-c', 
-                                os.path.join(self.temp_dir, "cache~"),
-                                'po',
-                                filename, 
-                                os.path.join(self.temp_dir, 
-                                             os.path.basename(filename))
-                               ])
-
     def read_jobs(self, job_directory):
         messages = []
-        for filename in path_expand_recursive(job_directory):
+        if os.environ.get("CHECKBOX_PACKAGING",0):
+            job_path = "./build/share/checkbox/jobs"
+        else:
+            job_path = "./jobs"
+
+        for filename in path_expand_recursive(job_path):
             if not filename.endswith("~"):
                 file = open(filename, "r", encoding="utf-8")
                 template = TemplateI18n()
@@ -54,14 +46,11 @@ class MessageFileFormatTest(unittest.TestCase):
 
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
-        self.i18n_merge_jobs(self.temp_dir)
         self.messages = self.read_jobs(self.temp_dir)
 
     def tearDown(self):
         if self.temp_dir:
-            for filename in path_expand_recursive(self.temp_dir):
-                os.unlink(filename)
-            os.rmdir(self.temp_dir)
+            shutil.rmtree(self.temp_dir)
             self.temp_dir = None
 
     def test_job_files_valid(self):
