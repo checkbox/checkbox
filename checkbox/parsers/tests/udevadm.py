de051bbe9764170904f7af88b9b96698e3430a11
Daniel Manrique <daniel.manrique@canonical.com>
2012-07-23 11:09:20 -0400

    Add whitespace stripping test

--- checkbox/parsers/tests/udevadm.py
+++ checkbox/parsers/tests/udevadm.py
@@ -74,3 +74,6 @@ class TestDecodeId(TestCase):
     
     def test_escape(self):
         self.assertEqual("USB 2.0", decode_id("USB\\x202.0"))
+
+    def test_strip_whitespace(self):
+        self.assertEqual("USB 2.0", decode_id("  USB 2.0  "))
