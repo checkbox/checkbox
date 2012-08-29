c8c9019d5f63fdbcad856e4bef8637cf36c23254
Brendan Donegan <brendan.donegan@canonical.com>
2012-08-29 16:06:43 +0100

    [FEATURE] Add environment_info plugin which sets environment variables according to the values set in the plugin via Checkboxes INI files.

---
+++ plugins/environment_info.py
@@ -0,0 +1,41 @@
+#
+# This file is part of Checkbox.
+#
+# Copyright 2012 Canonical Ltd.
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+
+from checkbox.plugin import Plugin
+from checkbox.properties import String
+
+class EnvironmentInfo(Plugin):
+
+    routers = String(default="single")
+    router_ssid = String(default="")
+    router_psk = String(default="")
+
+    def register(self, manager):
+        super(EnvironmentInfo, self).register(manager)
+
+        self._manager.reactor.call_on("prompt-begin", self.prompt_begin, 100)
+
+    def prompt_begin(self, interface):
+        os.environ['ROUTERS'] = self.routers
+        os.environ['ROUTER_SSID'] = self.router_ssid
+        os.environ['ROUTER_PSK'] = self.router_psk
+
+factory = EnvironmentInfo
