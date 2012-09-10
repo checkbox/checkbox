b80de8d9e97983a083185f1d85c29c849ab03411
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2012-09-10 11:06:22 +0200

    Add connect_to_system_bus() to checkbox.dbus
    This small utility function standardizes how we get a dbus connection to the
    system bus. It's rather small but it does things properly and saves on a number
    of obscure imports.

--- checkbox/dbus/__init__.py
+++ checkbox/dbus/__init__.py
@@ -1,6 +1,8 @@
 # This file is part of Checkbox.
 #
 # Copyright 2012 Canonical Ltd.
+# Written by:
+#   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
 #
 # Checkbox is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,3 +23,31 @@ checkbox.dbus
 
 Utility modules for working with various things accessible over dbus
 """
+
+import logging
+
+from dbus import SystemBus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GObject
+
+
+def connect_to_system_bus():
+    """
+    Connect to the system bus properly.
+
+    Returns a tuple (system_bus, loop) where loop is a GObject.MainLoop
+    instance. The loop is there so that you can listen to signals.
+    """
+    # We'll need an event loop to observe signals. We will need the instance
+    # later below so let's keep it. Note that we're not passing it directly
+    # below as DBus needs specific API. The DBusGMainLoop class that we
+    # instantiate and pass is going to work with this instance transparently.
+    #
+    # NOTE: DBus tutorial suggests that we should create the loop _before_
+    # connecting to the bus.
+    logging.debug("Setting up glib-based event loop")
+    loop = GObject.MainLoop()
+    # Let's get the system bus object. We need that to access UDisks2 object
+    logging.debug("Connecting to DBus system bus")
+    system_bus = SystemBus(mainloop=DBusGMainLoop())
+    return system_bus, loop
