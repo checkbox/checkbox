968fd76e0697974cde9a6b483342384586a5d9c8
Daniel Manrique <daniel.manrique@canonical.com>
2012-10-26 10:57:22 -0400

    fixed dumb lint warning

--- checkbox/tests/test_message_files.py
+++ checkbox/tests/test_message_files.py
@@ -62,7 +62,7 @@ class MessageFileFormatTest(unittest.TestCase):
     def test_shell_jobs_with_root_have_needed_environ(self):
         import re
         shell_variables_regex = r'\$\{?([A-Z_]+)\}?'
-        environ_variables_regex= r'([A-Z_]+)'
+        environ_variables_regex = r'([A-Z_]+)'
         for message in self.messages:
             if message['plugin'] == 'shell' and 'user' in message:
                 shell_variables = []
