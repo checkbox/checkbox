9b6efc6e7021dad76174d1e53e108c9096e90b36
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2012-11-19 11:27:08 +0100

    Add mk-venv.sh
    This simple scripts assists in creating a virtualenv suitable for
    running and testing plainbox
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ mk-venv.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Create a virtualenv in /ramdisk
+#
+# This is how Zygmunt Krynicki works, feel free to use or adjust to your needs
+
+VENV_PATH=/ramdisk/venv
+
+if [ ! -d $(dirname $VENV_PATH) ]; then
+    echo "This script requires $(dirname $VENV_PATH) directory to exit"
+    exit 1
+fi
+
+if [ ! -d $VENV_PATH ]; then
+    virtualenv -p python3 $VENV_PATH
+    . $VENV_PATH/bin/activate
+    easy_install -U distribute
+    easy_install -U coverage
+    python3 setup.py develop
+fi
+
+echo "To activate your virtualenv run"
+echo " $ . $VENV_PATH/bin/activate"
