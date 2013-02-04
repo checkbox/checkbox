ec4866edd9b61dd2a6605df4f46062229f7a17bd
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-02-04 12:34:19 +0100

    vagrant: add test-in-vagrant
    This script allows one to quickly (relatively) run unit tests on all
    supported Ubuntu versions, right from their development machine, without
    even committing anything to the version control system.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ test-in-vagrant.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Run all tests in various versions of Ubuntu via vagrant
+
+mkdir -p vagrant-logs
+
+test -z $(which vagrant) && echo "You need to install vagrant first" && exit
+# XXX: this list needs to be in sync with Vagrantfile
+target_list="precise quantal raring"
+for target in $target_list; do
+    # Bring up target if needed
+    if ! vagrant status $target | grep -q running; then
+        vagrant up $target || ( echo "Unable to bring $target up [$?]" && continue )
+    fi
+    # Run checkbox unit tests
+    if vagrant ssh $target -c 'cd checkbox && ./test' >vagrant-logs/$target.checkbox.log 2>vagrant-logs/$target.checkbox.err; then
+        echo "CheckBox test suite [$target]: pass"
+    else
+        echo "CheckBox test suite [$target]: fail"
+    fi
+    # Run plainbox unit tests
+    # TODO: It would be nice to support fast failing here
+    if vagrant ssh $target -c 'cd checkbox/plainbox && python3 setup.py test' >vagrant-logs/$target.plainbox.log 2>vagrant-logs/$target.plainbox.err; then
+        echo "PlainBox test suite [$target]: pass"
+    else
+        echo "PlainBox test suite [$target]: fail"
+    fi
+    # Run plainbox integration test suite (that tests checkbox scripts)
+    if vagrant ssh $target -c 'plainbox self-test --verbose --fail-fast' >vagrant-logs/$target.self-test.log 2>vagrant-logs/$target.self-test.err; then
+        echo "Integration tests [$target]: pass"
+    else
+        echo "Integration tests [$target]: fail"
+    fi
+done
