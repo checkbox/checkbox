75038d932cd33bc2a54818a766bdf5f461f17656
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-03-04 14:53:03 +0100

    vagrant: display all output in uniform manner
    All output from test-in-vagrant.sh now looks like [$target]: msg
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- test-in-vagrant.sh
+++ test-in-vagrant.sh
@@ -10,32 +10,44 @@ target_list="precise quantal"
 for target in $target_list; do
     # Bring up target if needed
     if ! vagrant status $target | grep -q running; then
-        vagrant up $target || ( echo "Unable to bring $target up [$?]" && continue )
+        echo "[$target] Bringing VM 'up'"
+        if ! vagrant up $target >vagrant-logs/$target.startup.log 2>vagrant-logs/$target.startup.err; then
+            outcome=1
+            echo "[$target] Unable to 'up' VM!"
+            echo "[$target] NOTE: unable to execute tests, marked as failed"
+            continue
+        fi
     fi
+    # Display something before the first test output
+    echo "[$target] Starting tests..."
     # Run checkbox unit tests
     if vagrant ssh $target -c 'cd checkbox && ./test' >vagrant-logs/$target.checkbox.log 2>vagrant-logs/$target.checkbox.err; then
-        echo "CheckBox test suite [$target]: pass"
+        echo "[$target] CheckBox test suite: pass"
     else
-        echo "CheckBox test suite [$target]: fail"
-    outcome=1
+        outcome=1
+        echo "[$target] CheckBox test suite: fail"
     fi
     # Run plainbox unit tests
     # TODO: It would be nice to support fast failing here
     if vagrant ssh $target -c 'cd checkbox/plainbox && python3 setup.py test' >vagrant-logs/$target.plainbox.log 2>vagrant-logs/$target.plainbox.err; then
-        echo "PlainBox test suite [$target]: pass"
+        echo "[$target] PlainBox test suite: pass"
     else
-        echo "PlainBox test suite [$target]: fail"
-    outcome=1
+        outcome=1
+        echo "[$target] PlainBox test suite: fail"
     fi
     # Run plainbox integration test suite (that tests checkbox scripts)
     if vagrant ssh $target -c 'sudo plainbox self-test --verbose --fail-fast --integration-tests' >vagrant-logs/$target.self-test.log 2>vagrant-logs/$target.self-test.err; then
-        echo "Integration tests [$target]: pass"
+        echo "[$target] Integration tests: pass"
     else
-        echo "Integration tests [$target]: fail"
-    outcome=1
+        outcome=1
+        echo "[$target] Integration tests: fail"
     fi
     # Suspend the target to conserve resources
-    vagrant suspend $target || ( echo "Unable ta suspend $target  [$?]" && continue )
+    echo "[$target] Suspending VM 'up'"
+    if ! vagrant suspend $target >vagrant-logs/$target.suspend.log 2>vagrant-logs/$target.suspend.err; then
+        echo "[$target] Unable to suspend VM!"
+        echo "[$target] You may need to manually 'vagrant destroy $target' to fix this"
+    fi
 done
 # Propagate failure code outside
 exit $outcome
