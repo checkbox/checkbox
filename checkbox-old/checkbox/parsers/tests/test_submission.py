22df5a8ccc18a44d15d249d9195c065ca68022d5
Daniel Manrique <roadmr@ubuntu.com>
2013-08-20 09:14:21 -0400

    checkbox:parsers:tests:test_udevadm: Changes to test expectations from previous commit, since most systems sprouted a few AUDIO devices (that were previously OTHER or None)
    Note that we're mostly just changing the quantities of expected devices,
    since the categorization changes mean that, compared to the parser
    overhaul from rev 2235, we are finding a few extra devices. mostly these
    comprise audio and tty devices. A few of the expected devices changed
    their index, so we update that as well.
    
    None of the devices changed buses. Also, none of the "interesting"
    device categories (except for AUDIO) changed their number of devices,
    any device movement happened between OTHER and "None", per the linked
    bug which requests assigning some devices to category: None.

--- checkbox-old/checkbox/parsers/tests/test_submission.py
+++ checkbox-old/checkbox/parsers/tests/test_submission.py
@@ -151,7 +151,7 @@ class TestSubmissionParser(TestCase):
         """Device states can be in the udev element."""
         result = self.getResult("submission_udev.xml")
         self.assertTrue("device_states" in result)
-        self.assertEqual(len(result["device_states"]), 79)
+        self.assertEqual(len(result["device_states"]), 80)
 
     def test_device_udev_armhf(self):
         """ Ensure that device states from udev are also obtained
@@ -166,7 +166,7 @@ class TestSubmissionParser(TestCase):
         """Device states can be in a udevadm info element."""
         result = self.getResult("submission_info_udevadm.xml")
         self.assertTrue("device_states" in result)
-        self.assertEqual(len(result["device_states"]), 79)
+        self.assertEqual(len(result["device_states"]), 80)
 
     def test_device_dmidecode(self):
         """Device states can be in a dmidecode info element."""
