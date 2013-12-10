13960b43fa6ec2d54df73954d12cebd8dab38261
Brendan Donegan <brendan.donegan@canonical.com>
2013-12-10 17:34:07 +0000

    Remove code that filters whitelists.

--- checkbox-gui/gui-engine/PBNames.h
+++ checkbox-gui/gui-engine/PBNames.h
@@ -65,7 +65,4 @@ static const QString JobResult_OUTCOME_FAIL = "fail";
 static const QString JobResult_OUTCOME_SKIP = "skip";
 static const QString JobResult_OUTCOME_NONE = "none";
 
-// Acceptable whitelist prefix
-static const QString WhiteListPrefix = "ihv-";
-
 #endif // PB_NAMES_H
