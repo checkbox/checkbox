39a8067cba92f2eefc9b50ab30289ab61579141d
Sylvain Pineau <sylvain.pineau@canonical.com>
2014-03-31 23:15:26 +0200

    certification-client: Replace setup.py by manage.py

---
+++ providers/2013.com.canonical.certification.certification-client/manage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from plainbox.provider_manager import setup, N_
+
+setup(
+    name='2013.com.canonical.certification:certification-client',
+    version="1.0",
+    description=N_("The 2013.com.canonical.certification:certification-client provider"),
+    gettext_domain="2013_com_canonical_certification_certification-client",
+)
