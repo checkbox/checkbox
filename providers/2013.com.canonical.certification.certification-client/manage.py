93b1531bbf143eaa2569eb02b0d4ee07bbbce177
Sylvain Pineau <sylvain.pineau@canonical.com>
2014-04-01 15:49:55 +0200

    certification-client: drop the .provider file
    This file is created on demand by manage.py so we should not keep it.
    The same meta-data was moved to manage.py

--- providers/2013.com.canonical.certification.certification-client/manage.py
+++ providers/2013.com.canonical.certification.certification-client/manage.py
@@ -4,6 +4,6 @@ from plainbox.provider_manager import setup, N_
 setup(
     name='2013.com.canonical.certification:certification-client',
     version="1.0",
-    description=N_("The 2013.com.canonical.certification:certification-client provider"),
+    description=N_("Client Certification provider"),
     gettext_domain="2013_com_canonical_certification_certification-client",
 )
