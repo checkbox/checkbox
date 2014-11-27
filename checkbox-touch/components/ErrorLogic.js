ac29903bca9143620269910781f4f92c2c9ef872
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2014-11-27 19:33:02 +0100

    checkbox-touch: add finalLabel parameter to error popup
    This patch adds possibility to set the label of the button on (bottom-most)
    error popup. If not supplied, it'll still show 'Quit'.
    
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-touch/components/ErrorLogic.js
+++ checkbox-touch/components/ErrorLogic.js
@@ -1,6 +1,6 @@
 .import Ubuntu.Components.Popups 1.0 as Popups
 
-function showError(caller, errorMessage, continuation) {
+function showError(caller, errorMessage, continuation, finalLabel) {
     // stackDepth keeps track of how many popups are stacked on the screen
     // we need this so continuation is called only if the last (the bottom one) popup
     // is closed
@@ -10,7 +10,7 @@ function showError(caller, errorMessage, continuation) {
     if (showError.stackDepth > 1) {
         popup.buttonLabel = i18n.tr("Continue")
     } else {
-        popup.buttonLabel = i18n.tr("Quit")
+        popup.buttonLabel = finalLabel || i18n.tr("Quit")
         popup.done.connect(function() {
             continuation();
         });
