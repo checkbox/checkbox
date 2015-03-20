4b65b13bd917ed5623b26e73c607eac560266b31
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2015-03-20 12:51:42 +0100

    checkbox-touch: make APP_DIR fall back to '.'
    This patch makes Checkbox-Touch expand APP_DIR envvar to '.' when it's absent.
    This makes loading of local providers on desktop work as expected.
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-touch/py/checkbox_touch.py
+++ checkbox-touch/py/checkbox_touch.py
@@ -786,7 +786,7 @@ class CheckboxTouchApplication(PlainboxApplication):
         provider_list = all_providers.get_all_plugin_objects()
         # when running on ubuntu-touch device, APP_DIR env var is present
         # and points to touch application top directory
-        path = os.path.join(os.path.expandvars('$APP_DIR'),
+        path = os.path.join(os.path.normpath(os.getenv('APP_DIR', '.')),
                             os.path.normpath(providers_dir))
         _logger.info("Loading all providers from %s", path)
         if os.path.exists(path):
