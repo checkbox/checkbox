3636a097c31c3a6318dce5aa88fdad0c5d19d0f0
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2015-04-01 22:28:59 +0200

    checkbox-touch: add helper functions for autopilot testing
    This patch adds funcitonas that ease the process of implementing complex
    autopilot tests by outlining some of the common operations.
    
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-touch/tests/autopilot/checkbox_touch/test_checkbox_touch.py
+++ checkbox-touch/tests/autopilot/checkbox_touch/test_checkbox_touch.py
@@ -5,8 +5,35 @@ from autopilot.matchers import Eventually
 
 import checkbox_touch
 
+
 class TestCheckboxTouch(checkbox_touch.ClickAppTestCase):
-    """Test checking if app launches"""
+
+    def skip_test(self):
+        """Skip current test using header action."""
+        self.main_view.get_header().click_action_button('skip')
+        dialog = self.app.wait_select_single(objectName='dialog')
+        yes_btn = dialog.select_single(objectName='yesButton')
+        self.pointing_device.click_object(yes_btn)
+
+    def process_sequence_of_clicks_on_pages(self, steps):
+        """
+        Do a sequence of clicks on simple page->component hierarchies.
+
+        :param steps:
+            sequence of (page-objectName, component-objectName) pairs to go
+            through.
+
+        Typical run of checkbox-touch requires user to go through a sequence of
+        pages that have pass/fail buttons on them. This function helps go
+        through a sequence like that.
+        """
+        for parent, component in steps:
+            self.app.wait_select_single(
+                objectName=parent, visible=True)
+            clickable = self.main_view.wait_select_single(
+                objectName=component, visible=True)
+            self.pointing_device.click_object(clickable)
+
     def test_launches(self):
         main_view = self.app.select_single(objectName='mainView')
         self.assertThat(main_view.visible, Eventually(Equals(True)))
