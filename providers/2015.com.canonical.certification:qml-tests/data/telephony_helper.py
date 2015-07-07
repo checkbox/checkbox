ce5b99442a129f0d2dbf9acdab0a76316140c37b
Jonathan Cave <jonathan.cave@canonical.com>
2015-07-07 10:13:23 +0100

    providers: qml-tests: adds missing license

--- providers/2015.com.canonical.certification:qml-tests/data/telephony_helper.py
+++ providers/2015.com.canonical.certification:qml-tests/data/telephony_helper.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+# Copyright 2015 Canonical Ltd.
+# Written by:
+#   Jonathan Cave <jonathan.cave@canonical.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 
@@ -9,7 +24,7 @@ _logger = logging.getLogger('checkbox.touch')
 
 
 def get_modems():
-    """Return a list of modems identified by their path name"""
+    """Return a list of modems identified by their path name."""
     paths = []
     bus = dbus.SystemBus()
     try:
@@ -29,7 +44,7 @@ def get_modems():
 
 
 def send_sms(modem_path, recipient, text):
-    """Use MessageManager to send a SMS message to a recipient"""
+    """Use MessageManager to send a SMS message to a recipient."""
     bus = dbus.SystemBus()
     try:
         mm = dbus.Interface(bus.get_object('org.ofono', modem_path),
