2331ffb2af3820a875d338225fb80bec6ef2b53d
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2015-07-31 15:07:18 +0200

    checkbox-touch: bump version to 1.2.1
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-touch/py/checkbox_touch.py
+++ checkbox-touch/py/checkbox_touch.py
@@ -147,7 +147,7 @@ class CheckboxTouchApplication(PlainboxApplication):
     response data to alter the user interface.
     """
 
-    __version__ = (1, 2, 0, 'final', 0)
+    __version__ = (1, 2, 1, 'final', 0)
 
     def __init__(self):
         if plainbox.__version__ < (0, 22):
