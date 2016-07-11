cc8a316b8a9eb1b5678a8d82b9cb22996177b09a
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2016-07-11 14:39:35 +0200

    checkbox-touch:components: add error reporting when creating ConfirmationDlg
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-touch/components/ConfirmationLogic.js
+++ checkbox-touch/components/ConfirmationLogic.js
@@ -1,5 +1,7 @@
 .import Ubuntu.Components.Popups 1.0 as Popups
 
+.import QtQuick 2.0 as QtQuick
+
 function confirmRequest(caller, options, continuation) {
     // if the question was answered before and user selected to
     // remember their selection - 'returning' true
@@ -7,7 +9,14 @@ function confirmRequest(caller, options, continuation) {
         continuation(true);
         return;
     }
-    var popup = Qt.createComponent(Qt.resolvedUrl("ConfirmationDialog.qml")).createObject(caller);
+    var component = Qt.createComponent(Qt.resolvedUrl("ConfirmationDialog.qml"));
+    if (component.status == QtQuick.Component.Error) {
+        var msg = i18n.tr("could not create ConfirmationDialog component\n'") + component.errorString();
+        console.error(msg);
+        ErrorLogic.showError(mainView, msg, Qt.quit, i18n.tr("Quit"));
+    }
+    var popup = component.createObject(caller);
+
     popup.withRemember = options.remember;
     popup.question = options.question;
     popup.answer.connect(function(result, remember) {
